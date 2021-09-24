bcd06744f948797f18ad8ba7d24f42252f32e892
Bruno000 <73971514+Bruno000@users.noreply.github.com>
2021-09-24 19:17:17 -0300

    criando microservico de votacao

---
+++ routes/buscarVotacao.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+
+// Lista todos de votação
+router.get('/votacao/:id', async(req, res, next) => {
+    let idTema = req.params.id
+    try {
+        // Consumindo as propostas de votação com base no ID do tema
+        const {
+            data
+        } = await axios('https://dadosabertos.camara.leg.br/api/v2/proposicoes/' + idTema)
+
+        res.status(200).json({
+
+            'Nomes das ementas': data,
+
+        });
+    } catch (error) {
+        console.error("Algum erro inesperado aconteceu: " + error)
+    }
+
+});
+
+module.exports = router;
