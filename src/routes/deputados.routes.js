523948a3d2c48b64e4703ca7dd4b06c414ce948d
LUCAS HENRIQUE ALVES SANTOS <lucashas27@gmail.com>
2021-11-05 20:26:32 -0300

    Implementação JWT no microservico

--- src/routes/deputados.routes.js
+++ src/routes/deputados.routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const controller = require('../controllers/deputadoController');
 
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
+
 const router = express.Router();
 
+router.use(ensureAuthenticated);
 router.get('/', controller.list);
 
 module.exports = router;
--- src/routes/person.routes.js
+++ src/routes/person.routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const controller = require('../controllers/personController');
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
 
 const router = express.Router();
+
+router.use(ensureAuthenticated);
 
 router.post('/', controller.post);
 router.put('/:id', controller.put);
